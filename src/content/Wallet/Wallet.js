5315313bc37dcc91185b54a1aa6d79f968b35563
nSedrickm <nsedrick101@gmail.com>
2021-04-03 08:55:38 +0100

    refactor: load stored tokens from API

--- src/content/Wallet/Wallet.js
+++ src/content/Wallet/Wallet.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import DashHeader from '../../components/DashHeader';
 import { DashCard } from '../../components/Card';
 import { getToken } from '../../services/auth.service';
-import { getProviders, getStoredTokens, getPlatformOauthToken } from '../../services/wallet.service';
+import { getProviders, getPlatformOauthToken } from '../../services/wallet.service';
 import { Accordion, AccordionItem, AccordionSkeleton, Button, InlineNotification } from 'carbon-components-react';
 import { Save16, TrashCan16 } from "@carbon/icons-react";
 
@@ -39,61 +39,58 @@ const Wallet = () => {
     useEffect(() => {
         getProviders(AUTH_KEY)
             .then(response => {
-                console.log(response.data);
-                setProviders(response.data.default_provider);
-                setTokens(response.data.user_provider);
+                let tokens = response.data.user_provider;
+                let providers = response.data.default_provider;
+
+                if (providers.length) {
+                    setProviders(providers);
+                }
+                if (tokens.length) {
+                    setTokens(tokens);
+                }
                 setAlert({ loading: false, notify: false });
+            })
+            .catch((error) => {
+                if (error.response) {
+                    /*
+                     * The request was made and the server responded with a
+                     * status code that falls out of the range of 2xx
+                     */
+                    notificationProps.kind = "error";
+                    notificationProps.title = "An error occurred";
+                    notificationProps.subtitle = "please try again";
+                    setAlert({ loading: false, notify: true });
+
+                } else if (error.request) {
+                    /*
+                     * The request was made but no response was received, `error.request`
+                     * is an instance of XMLHttpRequest in the browser and an instance
+                     * of http.ClientRequest in Node.js
+                     */
+                    console.log(error.request);
+                    notificationProps.kind = "error";
+                    notificationProps.title = "Oops sorry";
+                    notificationProps.subtitle = "Its an issue on our end. Please try again";
+                    setAlert({ loading: false, notify: true });
+                } else {
+                    // Something happened in setting up the request and triggered an Error
+                    notificationProps.kind = "info";
+                    notificationProps.title = "Tokens";
+                    notificationProps.subtitle = "There are currently no stored tokens";
+                    setAlert({ loading: false, notify: true });
+                }
             });
-
-        // getStoredTokens(AUTH_KEY, 'google')
-        //     .then(response => {
-        //         setTokens(response.data.user_provider);
-        //         setAlert({ loading: false, notify: true });
-        //     })
-        //     .catch((error) => {
-        //         // Error 😨
-        //         if (error.response) {
-        //             /*
-        //              * The request was made and the server responded with a
-        //              * status code that falls out of the range of 2xx
-        //              */
-        //             notificationProps.kind = "error";
-        //             notificationProps.title = "An error occurred";
-        //             notificationProps.subtitle = "please try again";
-        //             setAlert({ loading: false, notify: true });
-
-        //         } else if (error.request) {
-        //             /*
-        //              * The request was made but no response was received, `error.request`
-        //              * is an instance of XMLHttpRequest in the browser and an instance
-        //              * of http.ClientRequest in Node.js
-        //              */
-        //             console.log(error.request);
-        //             notificationProps.kind = "error";
-        //             notificationProps.title = "Network error";
-        //             notificationProps.subtitle = "an error occured";
-        //             setAlert({ loading: false, notify: true });
-        //         } else {
-        //             // Something happened in setting up the request and triggered an Error
-        //             notificationProps.kind = "info";
-        //             notificationProps.title = "Tokens";
-        //             notificationProps.subtitle = "There are currently no stored tokens";
-        //             setAlert({ loading: false, notify: true });
-        //         }
-        //     });
 
     }, [AUTH_KEY]);
 
 
 
     return (
-
         <div className="bx--grid">
             <div className="bx--row">
                 <DashHeader
                     title="Your"
-                    subt
-                    itle="Wallet"
+                    subtitle="Wallet"
                     description="Save your tokens for rainy days"
                     className="bx--col"
                 />
@@ -128,6 +125,7 @@ const Wallet = () => {
                                                         <br />
 
                                                         <Button
+                                                            size="sm"
                                                             renderIcon={Save16}
                                                             onClick={() => getGoogleToken()}
                                                         >
@@ -137,23 +135,7 @@ const Wallet = () => {
                                                 </Accordion>
                                             );
                                         default:
-                                            return (
-                                                <Accordion size="xl" key={provider.provider}>
-                                                    <AccordionItem title={provider.provider}>
-
-                                                        <h5>Platform</h5>
-                                                        <p>{provider.platform}</p>
-                                                        <br />
-
-                                                        <Button
-                                                            renderIcon={Save16}
-                                                            onClick={() => getGoogleToken()}
-                                                        >
-                                                            Store
-                                                        </Button>
-                                                    </AccordionItem>
-                                                </Accordion>
-                                            );
+                                            return <p>No available providers</p>
                                     }
                                 })
                                 }
@@ -172,36 +154,29 @@ const Wallet = () => {
                             <AccordionSkeleton open count={3} />
                             :
                             <>
-                                {/* {tokens ?
-                                    <Accordion size="xl">
-                                        <AccordionItem title="Google">
-                                            <h5>Platform</h5>
-                                            <p>{tokens.platform}</p>
-                                            <br />
-                                            <h5>Token</h5>
-                                            <p style={{ "wordWrap": "break-word" }}>
-                                                {tokens.token.access_token}
-                                            </p>
-                                            <br />
-                                            <h5>Expiry date</h5>
-                                            <p>{tokens.token.expiry_date}</p>
-                                            <br />
-                                            <h5>Scopes</h5>
-                                            {tokens.token.scope.map(scope => (
-                                                <p key={scope}>{scope}</p>
-                                            ))}
-                                            <br />
-                                            <Button
-                                                kind="danger"
-                                                renderIcon={TrashCan16}
-                                            >
-                                                Delete
+                                {tokens ?
+                                    <>
+                                        {tokens.map(token => (
+                                            <Accordion size="xl" key={token.provider}>
+                                                <AccordionItem title={token.provider}>
+                                                    <h5>Platform</h5>
+                                                    <p>{token.platform}</p>
+                                                    <br />
+                                                    <Button
+                                                        kind="danger"
+                                                        size="sm"
+                                                        renderIcon={TrashCan16}
+                                                    >
+                                                        Delete
                                             </Button>
-                                        </AccordionItem>
-                                    </Accordion>
+                                                </AccordionItem>
+                                            </Accordion>
+                                        ))}
+                                    </>
                                     :
                                     <p>No tokens stored</p>
-                                } */}
+
+                                }
                             </>
                         }
                     </DashCard>
