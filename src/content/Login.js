cf3bb67af6c307b1dfd7a670181a26ff0e673e1e
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-10 13:45:49 +0100

    update: update login component to new desing

--- src/content/Login.js
+++ src/content/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import { Container as ContainerBase } from "components/misc/Layouts";
 import tw from "twin.macro";
@@ -6,7 +6,10 @@ import styled from "styled-components";
 import illustration from "images/login-illustration.svg";
 import logo from "images/logo.png";
 import { FiLogIn } from "react-icons/fi";
-import { Button, TextInputField } from 'evergreen-ui'
+import { Button, TextInputField, toaster } from 'evergreen-ui';
+
+import { setToken, userLogin } from 'services/auth.service';
+import { Link } from "react-router-dom";
 
 const Container = tw(ContainerBase)`min-h-screen bg-primary-900 text-white font-medium flex justify-center -m-8`;
 const Content = tw.div`max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg flex justify-center flex-1`;
@@ -24,56 +27,142 @@ const IllustrationImage = styled.div`
   ${tw`m-12 xl:m-16 w-full max-w-sm bg-contain bg-center bg-no-repeat`}
 `;
 
-const Login = () => (
+const Login = ({ setIsLoggedIn }) => {
 
-  <AnimationRevealPage>
-    <Container>
-      <Content>
-        <MainContainer>
-          <LogoImage src={logo} />
-          <MainContent>
-            <Heading>Sign In</Heading>
-            <FormContainer>
-              <Form>
-                <Input
-                  type="text"
-                  label="Phone number"
-                  placeholder="Phone number"
-                  inputHeight={40}
-                  required
-                />
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(false);
 
-                <Input
-                  type="password"
-                  label="Password"
-                  placeholder="Password"
-                  inputHeight={40}
-                  required
-                />
+  const formProps = {
+    onSubmit: async (e) => {
+      e.preventDefault();
+      setLoading(true);
+      userLogin(username, password)
+        .then(response => {
+          if (response.status === 200) {
+            setLoading(false);
+            toaster.success('Login successful');
+            setTimeout(() => {
+              //use placeholder token value for now
+              setToken(response.data.auth_key);
+              setIsLoggedIn(true);
+            }, 3000);
+          }
+        })
+        .catch(error => {
+          if (error.response) {
+            /*
+             * The request was made and the server responded with a
+             * status code that falls out of the range of 2xx
+             */
+            switch (error.response.status) {
+              case 400:
+                setLoading(false);
+                toaster.danger('Something went wrong', {
+                  description: 'Its not you its Us. Please try again'
+                }
+                );
+                break;
 
-                <SubmitButton
-                  type="submit"
-                  appearance="primary"
-                  height={40}
-                  iconBefore={FiLogIn}>
-                  <span className="text">Sign In</span>
-                </SubmitButton>
-              </Form>
-              <p tw="mt-8 text-sm text-gray-600 text-center">
-                Dont have an account?{" "}
-                <a href="/sign-up">
-                  Sign Up
-                 </a>
-              </p>
-            </FormContainer>
-          </MainContent>
-        </MainContainer>
-        <IllustrationContainer>
-          <IllustrationImage imageSrc={illustration} />
-        </IllustrationContainer>
-      </Content>
-    </Container>
-  </AnimationRevealPage>
-);
+              case 401:
+                setLoading(false);
+                toaster.danger('Forbidden', {
+                  description: 'Account is unauthorized. Sign Up to create account'
+                }
+                );
+                break;
+
+              case 500:
+                setLoading(false);
+                toaster.danger('Something went wrong', {
+                  description: 'Its not you its Us. We are working to resolve this'
+                }
+                );
+                break;
+
+              default:
+                setLoading(false);
+            }
+
+          } else if (error.request) {
+            /*
+             * The request was made but no response was received, `error.request`
+             * is an instance of XMLHttpRequest in the browser and an instance
+             * of http.ClientRequest in Node.js
+             */
+            setLoading(false);
+            toaster.danger('Network error', {
+              description: 'Please check your network and try again'
+            }
+            );
+
+          } else {
+            // Something happened in setting up the request and triggered an Error
+            setLoading(false);
+            toaster.danger('Network error', {
+              description: 'Please check your network and try again'
+            }
+            );
+          }
+        });
+    },
+  };
+
+  return (
+    <AnimationRevealPage>
+      <Container>
+        <Content>
+          <MainContainer>
+            <LogoImage src={logo} />
+            <MainContent>
+              <Heading>Sign In</Heading>
+              <FormContainer>
+                <Form {...formProps}>
+                  <Input
+                    type="text"
+                    label="Phone number"
+                    placeholder="Phone number"
+                    inputHeight={40}
+                    required
+                    onChange={evt => setUsername(evt.target.value)}
+                  />
+
+                  <Input
+                    type="password"
+                    label="Password"
+                    placeholder="Password"
+                    inputHeight={40}
+                    required
+                    onChange={evt => setPassword(evt.target.value)}
+                  />
+
+
+                  <SubmitButton
+                    type="submit"
+                    appearance="primary"
+                    height={40}
+                    iconBefore={loading ? null : FiLogIn}
+                    isLoading={loading}
+                  >
+                    <span className="text">Sign In </span>
+                  </SubmitButton>
+                </Form>
+                <p tw="mt-8 text-sm text-gray-600 text-center">
+                  Dont have an account?{" "}
+                  <Link to="/sign-up">
+                    Sign Up
+                 </Link>
+                </p>
+              </FormContainer>
+            </MainContent>
+          </MainContainer>
+          <IllustrationContainer>
+            <IllustrationImage imageSrc={illustration} />
+          </IllustrationContainer>
+        </Content>
+      </Container>
+    </AnimationRevealPage>
+  )
+};
 
 export default Login;
