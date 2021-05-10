ac4867795499d74a5c8d62a1d5d4f47d1120a2ec
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-10 10:45:44 +0100

    refactor: update login

--- src/content/Login.js
+++ src/content/Login.js
@@ -10,18 +10,15 @@ import { Button, TextInputField } from 'evergreen-ui'
 
 const Container = tw(ContainerBase)`min-h-screen bg-primary-900 text-white font-medium flex justify-center -m-8`;
 const Content = tw.div`max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg flex justify-center flex-1`;
-const MainContainer = tw.div`lg:w-1/2 xl:w-5/12 p-6 sm:p-12`;
-const LogoLink = tw.a``;
+const MainContainer = tw.div`lg:w-1/2 xl:w-5/12 p-2 sm:p-12`;
 const LogoImage = tw.img`h-60 mx-auto block`;
 const MainContent = tw.div`flex flex-col items-center`;
-const Heading = tw.h1`text-2xl xl:text-3xl font-extrabold`;
+const Heading = tw.h1`text-2xl xl:text-3xl font-bold`;
 const FormContainer = tw.div`w-full flex-1 mt-8`;
-
-
 const Form = tw.form`mx-auto`;
 const Input = tw(TextInputField)`w-full rounded-lg`;
 const SubmitButton = tw(Button)`w-full rounded-lg`;
-const IllustrationContainer = tw.div`sm:rounded-r-lg flex-1 bg-purple-100 text-center hidden lg:flex justify-center`;
+const IllustrationContainer = tw.div`sm:rounded-r-lg flex-1 bg-primary-400 text-center hidden lg:flex justify-center`;
 const IllustrationImage = styled.div`
   ${props => `background-image: url("${props.imageSrc}");`}
   ${tw`m-12 xl:m-16 w-full max-w-sm bg-contain bg-center bg-no-repeat`}
@@ -33,9 +30,7 @@ const Login = () => (
     <Container>
       <Content>
         <MainContainer>
-          <LogoLink href="#">
-            <LogoImage src={logo} />
-          </LogoLink>
+          <LogoImage src={logo} />
           <MainContent>
             <Heading>Sign In</Heading>
             <FormContainer>
