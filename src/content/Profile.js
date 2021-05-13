546f75bd9c338dfff56d62618bf7055a9b0a6387
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-13 22:58:14 +0100

    refactor: add section container

--- src/content/Profile.js
+++ src/content/Profile.js
@@ -2,9 +2,10 @@ import React from 'react';
 import tw from "twin.macro"
 import { Button } from "evergreen-ui";
 import { IoMdSync } from "react-icons/io";
+import PageAnimationWrapper from "helpers/PageAnimationWrapper";
 
 const SyncButton = tw(Button)`rounded-md`;
-const SectionContainer = tw.section`text-gray-600 font-bold md:p-8`;
+const SectionContainer = tw.section`container mx-auto flex px-5 md:px-8 py-24 md:flex-row flex-col items-center font-bold`;
 const ImageContainer = tw.div`lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0`
 const DetailsContainer = tw.div`lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center`;
 const Image = tw.img`object-cover object-center rounded`;
@@ -14,8 +15,8 @@ const Description = tw.p`mb-8 leading-relaxed`;
 const Profile = () => {
     return (
         <>
-            <SectionContainer>
-                <div tw="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <PageAnimationWrapper>
+                <SectionContainer>
                     <ImageContainer>
                         <Image alt="hero" src="https://dummyimage.com/720x600" />
                     </ImageContainer>
@@ -30,8 +31,8 @@ const Profile = () => {
                             Sync
                             </SyncButton>
                     </DetailsContainer>
-                </div>
-            </SectionContainer>
+                </SectionContainer>
+            </PageAnimationWrapper>
         </>
     );
 };
