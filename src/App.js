d47bedda7b0890c970de90b46e8aab791306f1fb
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-14 17:40:29 +0100

    chore : update inports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import './App.scss';
+import './App.css';
 import "rsuite/dist/styles/rsuite-default.css";
 import "tailwindcss/dist/base.css";
 import Login from "content/Login";
