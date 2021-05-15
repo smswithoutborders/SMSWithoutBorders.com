131be9e983cb245b85591c47d3a13e35bce79aa2
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-15 10:39:53 +0100

    update: display profile info on login

--- src/content/Profile.js
+++ src/content/Profile.js
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import tw from "twin.macro"
-import { Button, Avatar} from "evergreen-ui";
+import { Button, Avatar } from "evergreen-ui";
 import { IoMdSync } from "react-icons/io";
 import PageAnimationWrapper from "helpers/PageAnimationWrapper";
+
+import { getProfileInfo } from "services/profile.service";
 
 const SyncButton = tw(Button)`rounded-md`;
 const SectionContainer = tw.section`container mx-auto flex px-5 md:px-8 py-24 md:flex-row flex-col items-center font-bold`;
 const ImageContainer = tw.div`lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0`
 const DetailsContainer = tw.div`lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left items-center text-center`;
 const Image = tw(Avatar)`object-cover object-center block mx-auto`;
-const Heading = tw.h1`font-medium sm:text-4xl text-3xl mb-4 font-medium text-gray-900`;
-const Description = tw.p`mb-8 leading-relaxed text-gray-900`;
+const Heading = tw.h1`font-bold sm:text-4xl text-3xl mb-4 font-bold text-gray-900`;
+const Description = tw.h3`text-lg leading-relaxed text-gray-800`;
+const Meta = tw.p`font-light leading-relaxed text-gray-700 mb-4`;
 
 const Profile = () => {
+
+    const [profile, setProfile] = useState();
+
+    useEffect(() => {
+        getProfileInfo()
+            .then(response => {
+                setProfile(response.data);
+            })
+    }, []);
+
+
+
     return (
         <>
             <PageAnimationWrapper>
                 <SectionContainer>
                     <ImageContainer>
-                        <Image name="User Name" size={250} />
+                        <Image name={profile?.phone_number} size={250} />
                     </ImageContainer>
                     <DetailsContainer>
-                        <Heading> Welcome, (User Name) </Heading>
-                        <Description>Account details go here</Description>
+                        <Heading> Welcome, {profile?.phone_number} </Heading>
+                        <Description>Join Date</Description>
+                        <Meta>{new Date(profile?.created).toLocaleString()}</Meta>
+                        <br />
+                        <Description>Last Login</Description>
+                        <Meta>{new Date(profile?.last_login).toLocaleString()}</Meta>
+                        <br />
                         <SyncButton
                             iconBefore={IoMdSync}
                             appearance="primary"
