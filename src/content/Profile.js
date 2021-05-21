2f4d2557412a0a441bed4b44b15a985563bdc7c7
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-21 15:28:39 +0100

    feat: add qr code

--- src/content/Profile.js
+++ src/content/Profile.js
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import tw from "twin.macro"
 import { Button, Avatar, toaster, Spinner, Pane } from "evergreen-ui";
-import { IoMdSync } from "react-icons/io";
+import { IoMdSync, IoMdArrowBack } from "react-icons/io";
+import QRCode from 'qrcode.react';
 import PageAnimationWrapper from "helpers/PageAnimationWrapper";
-
 import { getProfileInfo, getProfile, setProfile } from "services/profile.service";
 
 const SyncButton = tw(Button)`rounded-md`;
 const SectionContainer = tw.section`container mx-auto flex px-5 md:px-8 py-24 md:flex-row flex-col items-center font-bold`;
-const ImageContainer = tw.div`lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0`
-const DetailsContainer = tw.div`lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left items-center text-center`;
+const ImageContainer = tw.div`md:w-1/2 mb-10 md:mb-0`
+const DetailsContainer = tw.div`md:w-1/2 flex flex-col md:items-start md:text-left items-center text-center`;
 const Image = tw(Avatar)`object-cover object-center block mx-auto`;
 const Heading = tw.h1`font-bold sm:text-4xl text-3xl mb-4 font-bold text-gray-900`;
 const Description = tw.h3`text-lg leading-relaxed text-gray-800`;
+const QRContainer = tw(QRCode)`object-cover object-center block mx-auto`;
 const Meta = tw.p`font-light leading-relaxed text-gray-700 mb-4`;
 
 const Profile = () => {
 
     const [profile, setProfileState] = useState(getProfile);
     const [loading, setLoading] = useState(false);
+    const [sync, setSync] = useState(false);
 
     useEffect(() => {
         if (!profile) {
@@ -63,40 +65,72 @@ const Profile = () => {
         }
     }, [profile]);
 
-    return (
-        <>
-            <PageAnimationWrapper>
 
-                {loading ? (
-                    <Pane display="flex" alignItems="center" justifyContent="center" height={400}>
-                        <Spinner />
-                    </Pane>
-                ) : (
-                        <SectionContainer>
-                            <ImageContainer>
-                                <Image name={profile?.phone_number} size={250} />
-                            </ImageContainer>
-                            <DetailsContainer>
-                                <Heading> Welcome, {profile?.phone_number} </Heading>
-                                <Description>Join Date</Description>
-                                <Meta>{new Date(profile?.created).toLocaleString()}</Meta>
-                                <br />
-                                <Description>Last Login</Description>
-                                <Meta>{new Date(profile?.last_login).toLocaleString()}</Meta>
-                                <br />
-                                <SyncButton
-                                    iconBefore={IoMdSync}
-                                    appearance="primary"
-                                    height="40"
-                                >
-                                    Sync
+    if (loading) {
+        return (
+            <Pane display="flex" alignItems="center" justifyContent="center" height={400}>
+                <Spinner />
+            </Pane>
+        )
+    }
+
+    if (sync) {
+        return (
+            <PageAnimationWrapper>
+                <SectionContainer>
+                    <ImageContainer>
+                        <QRContainer
+                            value="http://smswithoutborders.com"
+                            size={250}
+                        />
+                    </ImageContainer>
+                    <DetailsContainer>
+                        <Heading>Sync Mobile</Heading>
+                        <Description>Please scan the QR code to sync with App</Description>
+                        <Meta>This lets you use the SW/OB secure gateway for all messages</Meta>
+                        <br />
+                        <Description>Status</Description>
+                        <Meta>pending</Meta>
+                        <br />
+                        <SyncButton
+                            iconBefore={IoMdArrowBack}
+                            appearance="default"
+                            height="40"
+                            onClick={() => setSync(!sync)}
+                        >
+                            profile
                         </SyncButton>
-                            </DetailsContainer>
-                        </SectionContainer>
-                    )
-                }
+                    </DetailsContainer>
+                </SectionContainer>
             </PageAnimationWrapper>
-        </>
+        )
+    }
+
+    return (
+        <PageAnimationWrapper>
+            <SectionContainer>
+                <ImageContainer>
+                    <Image name={profile?.phone_number} size={250} />
+                </ImageContainer>
+                <DetailsContainer>
+                    <Heading> Welcome, {profile?.phone_number} </Heading>
+                    <Description>Join Date</Description>
+                    <Meta>{new Date(profile?.created).toLocaleString()}</Meta>
+                    <br />
+                    <Description>Last Login</Description>
+                    <Meta>{new Date(profile?.last_login).toLocaleString()}</Meta>
+                    <br />
+                    <SyncButton
+                        iconBefore={IoMdSync}
+                        appearance="primary"
+                        height="40"
+                        onClick={() => setSync(!sync)}
+                    >
+                        sync
+                        </SyncButton>
+                </DetailsContainer>
+            </SectionContainer>
+        </PageAnimationWrapper>
     );
 };
 
