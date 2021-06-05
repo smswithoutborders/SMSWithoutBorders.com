3dcff34abc0d05dee5766c0b9b746ac61b4473a1
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-05 14:11:57 +0100

    update: add components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import Navbar from "components/Navbar";
 import MainNavbar from "components/MainNavbar";
+import { Text, HeadingRow, Heading, Description, DonateButton } from "components/misc/Text";
 
 export {
+    Text,
+    HeadingRow,
+    Heading,
+    Description,
+    DonateButton,
     Navbar,
     MainNavbar
 }
