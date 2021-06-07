4cb20b80e3bc73d8b076be0cb46d0ec25b428e41
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-07 07:08:47 +0100

    refactor: create App context for shared state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import "rsuite/dist/styles/rsuite-default.css";
@@ -7,40 +7,103 @@ import Login from "content/Login";
 import SignUp from "content/SignUp"
 import HomePage from 'content/HomePage';
 import DashBoard from "content/DashBoard";
-import { getToken } from "services/auth.service";
 import PrivacyPage from 'content/PrivacyPage';
+import { getLocalState, setLocalState, clearLocalState } from "services/storage.service";
+
+import { removeToken } from "services/auth.service";
+import { removeProfile } from "services/profile.service";
+
+const AppContext = React.createContext();
+export const useAppContext = () => React.useContext(AppContext);
+
+const Reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN": {
+      return {
+        ...state,
+        id: action.payload.id,
+        token: action.payload
+      }
+    }
+    case "SETUSERDATA": {
+      return {
+        ...state,
+        userData: action.payload
+      }
+    }
+    case "LOGOUT": {
+      return {
+        ...state,
+        id: action.payload.id,
+        token: action.payload.token,
+        userData: action.payload.userData
+      }
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`)
+    }
+  }
+};
+
+
+let localState = getLocalState();
+let initialState = localState || {
+  id: "",
+  token: "",
+  userData: {}
+}
+
 
 const App = () => {
 
-  let token = getToken();
-  let isLoggedIn = false;
+  const [state, dispatch] = useReducer(Reducer, initialState);
 
-  if (token) {
-    isLoggedIn = true;
+  const { token, userData } = state;
+
+  useEffect(() => {
+    setLocalState(state);
+    initialState = getLocalState();
+  }, [state])
+
+
+  const handleLogOut = () => {
+    //remove user token from session storage
+    removeToken();
+    removeProfile();
+    clearLocalState();
+    // return user to login
+    window.location.replace("/")
   }
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
+    <AppContext.Provider
+      value={{
+        userData,
+        dispatch,
+        handleLogOut
+      }}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
 
-        <Route exact path="/privacy-policy">
-          <PrivacyPage />
-        </Route>
+          <Route exact path="/privacy-policy">
+            <PrivacyPage />
+          </Route>
 
-        <Route path="/dashboard">
-          {isLoggedIn ? <DashBoard /> : <Redirect to="/login" />}
-        </Route>
-        <Route exact path="/login">
-          {isLoggedIn ? <Redirect to="/dashboard" /> : <Login />}
-        </Route>
-        <Route exact path="/sign-up">
-          <SignUp />
-        </Route>
-      </Switch>
-    </Router>
+          <Route path="/dashboard">
+            {token ? <DashBoard /> : <Redirect to="/login" />}
+          </Route>
+          <Route exact path="/login">
+            {token ? <Redirect to="/dashboard" /> : <Login />}
+          </Route>
+          <Route exact path="/sign-up">
+            <SignUp />
+          </Route>
+        </Switch>
+      </Router>
+    </AppContext.Provider>
   );
 };
 
