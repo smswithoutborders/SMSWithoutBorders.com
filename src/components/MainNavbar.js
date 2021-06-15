9ebffe91fbdeef0993f7b3c1e066659860b4f368
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-15 13:43:07 +0100

    update: main navbar

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -5,32 +5,33 @@ import styled from "styled-components";
 import { FiLogIn, FiUserPlus } from "react-icons/fi";
 
 import useAnimatedNavToggler from "helpers/useAnimatedNavToggler.js";
-import logo from "images/logo-icon-dark.png";
+import logo from "images/logo-icon-light.png";
 import { FiMenu as MenuIcon, FiX as CloseIcon } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 
-const MainHeader = tw.header`flex justify-between items-center bg-gray-900 shadow-2xl`;
+const MainHeader = tw.header`flex justify-between items-center bg-gray-100`;
 const NavLinks = tw.div`inline-flex`;
-const NavButton = tw.button`h-16 transition duration-300 hocus:bg-gray-700 hocus:outline-none hocus:text-white text-white font-medium  px-6 py-3 no-underline items-center appearance-none`;
-const NavLink = tw(Link)`inline-flex h-16 transition duration-300 hocus:bg-gray-700 hocus:outline-none hocus:text-white text-white font-medium  px-6 py-3 no-underline items-center hocus:no-underline appearance-none`;
-const ExtLink = tw.a`inline-flex h-16 transition duration-300 hocus:bg-gray-700 hocus:outline-none hocus:text-white text-white font-medium  px-6 py-3 no-underline items-center hocus:no-underline appearance-none`;
+const NavButton = tw.button`h-16 items-center transition duration-300 hocus:bg-gray-700 hocus:outline-none hocus:text-white text-gray-900 font-medium  px-6 py-3 no-underline appearance-none`;
+const NavLink = tw(Link)`inline-flex h-16 transition duration-300 hocus:bg-gray-700 hocus:outline-none hocus:text-white text-gray-900 font-medium  px-6 py-3 no-underline items-center hocus:no-underline appearance-none`;
+const ExtLink = tw.a`inline-flex h-16 transition duration-300 hocus:bg-gray-700 hocus:outline-none hocus:text-white text-gray-900 font-medium  px-6 py-3 no-underline items-center hocus:no-underline appearance-none`;
 const LogoLink = styled(NavLink)`
-  ${tw`inline-flex items-center ml-0! bg-primary-900 focus:bg-primary-900`};
+  ${tw`inline-flex items-center ml-0! hocus:bg-gray-100 hocus:text-gray-900 font-bold text-xl`};
   img {
     ${tw`w-8 h-8 mr-3`}
   }
 `;
 const UserActions = tw.div`flex flex-row items-center`;
-const MobileNavLinksContainer = tw.nav`flex flex-1 items-center justify-between`;
+const MobileNavContainer = tw.nav`flex flex-1 items-center justify-between`;
 const NavToggle = tw(NavButton)`lg:hidden focus:outline-none transition duration-300`;
-const MobileNavLinks = motion(styled.div`
+
+const MobileNav = motion(styled.div`
   ${tw`lg:hidden z-40 fixed top-28 inset-x-4 p-2 border text-center rounded-lg text-white bg-gray-900 shadow-2xl`}
   ${NavLinks} {
     ${tw`flex flex-col items-center`}
   }
   `);
-const DesktopNavLinks = tw.nav`hidden lg:flex flex-1 justify-between items-center bg-gray-900`;
-
+const DesktopNav = tw.nav`hidden lg:flex flex-1 justify-between items-center bg-gray-100`;
+const Divider = tw.span`text-gray-900`;
 
 const MainNavbar = () => {
 
@@ -48,7 +49,7 @@ const MainNavbar = () => {
                 <NavLink key="login" to="/login" onClick={toggleNavbar}>
                     <FiLogIn size={16} /> &nbsp; Log In
                 </NavLink>
-                <span>|</span>
+                <Divider>|</Divider>
                 <NavLink key="sign-up" to="/sign-up" onClick={toggleNavbar}>
                     <FiUserPlus size={16} /> &nbsp; Sign Up
                 </NavLink>
@@ -66,19 +67,19 @@ const MainNavbar = () => {
     return (
         <>
             <MainHeader>
-                <DesktopNavLinks css={collapseBreakpointCss.desktopNavLinks}>
+                <DesktopNav css={collapseBreakpointCss.desktopNavLinks}>
                     {defaultLogoLink}
                     {defaultLinks}
-                </DesktopNavLinks>
-                <MobileNavLinksContainer css={collapseBreakpointCss.mobileNavLinksContainer}>
+                </DesktopNav>
+                <MobileNavContainer css={collapseBreakpointCss.mobileNavLinksContainer}>
                     {defaultLogoLink}
-                    <MobileNavLinks initial={{ x: "150%", display: "none" }} animate={animation} css={collapseBreakpointCss.mobileNavLinks}>
+                    <MobileNav initial={{ x: "150%", display: "none" }} animate={animation} css={collapseBreakpointCss.mobileNavLinks}>
                         {defaultLinks}
-                    </MobileNavLinks>
+                    </MobileNav>
                     <NavToggle onClick={toggleNavbar} className={showNavLinks ? "open" : "closed"}>
                         {showNavLinks ? <CloseIcon tw="w-6 h-6" /> : <MenuIcon tw="w-6 h-6" />}
                     </NavToggle>
-                </MobileNavLinksContainer>
+                </MobileNavContainer>
             </MainHeader>
         </>
     );
