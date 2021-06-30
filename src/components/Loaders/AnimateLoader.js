a4f6920d46686c179a522cd6377b5f366241b009
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-30 18:02:08 +0100

    update: add inline loader component

--- src/components/Loaders/AnimateLoader.js
+++ src/components/Loaders/AnimateLoader.js
@@ -2,11 +2,22 @@ import React from "react";
 import tw from "twin.macro";
 import { Spinner } from "evergreen-ui";
 
-const LoadingContainer = tw.div`h-screen grid place-items-center`;
+const LoadingContainer = tw.div`grid place-items-center`;
 
 const AnimateLoader = () => {
     return (
-        <LoadingContainer>
+        <LoadingContainer tw="h-screen">
+            <div>
+                <Spinner tw="mx-auto" />
+                <p tw="mt-2">Loading please wait</p>
+            </div>
+        </LoadingContainer>
+    );
+}
+
+export const InlineLoader = () => {
+    return (
+        <LoadingContainer tw="h-80">
             <div>
                 <Spinner tw="mx-auto" />
                 <p tw="mt-2">Loading please wait</p>
