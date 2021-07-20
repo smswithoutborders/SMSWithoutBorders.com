c41079c491a11923ce4ca915fc9ae3b394e83fc6
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-07-20 20:59:41 +0100

    fix: google token storage

--- src/content/DashBoard.js
+++ src/content/DashBoard.js
@@ -5,7 +5,7 @@ import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import PageAnimationWrapper from "helpers/PageAnimationWrapper";
 import Profile from 'content/Profile';
 import Wallet from 'content/Wallet';
-import WalletRedirect from 'content/Wallet/TwitterRedirect';
+import GoogleRedirect from 'content/Wallet/GoogleRedirect';
 import TwitterRedirect from "content/Wallet/TwitterRedirect";
 import NotFoundPage from "content/NotFoundPage";
 import SettingsPage from "content/SettingsPage";
@@ -23,7 +23,7 @@ const DashBoard = () => {
           <Route exact path={`${path}/profile`} component={Profile} />
           <Route exact path={`${path}/settings`} component={SettingsPage} />
           <Route exact path={`${path}/wallet`} component={Wallet} />
-          <Route exact path={`${path}/oauth2/google/Tokens/redirect/`} component={WalletRedirect} />
+          <Route exact path={`${path}/oauth2/google/Tokens/redirect/`} component={GoogleRedirect} />
           <Route exact path={`${path}/oauth2/twitter/Tokens/redirect/`} component={TwitterRedirect} />
           <Route component={NotFoundPage} />
         </Switch>
