f57fbb4c654e5dbde83b10460a8682a0f30ba588
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-01-22 16:18:07 +0100

    update: add action to reset store state

---
+++ src/features/reducers.js
@@ -0,0 +1,28 @@
+// contains all store reducers
+import { combineReducers } from "@reduxjs/toolkit";
+import { API } from "../services/api";
+import authReducer from "./auth";
+import profileReducer from "./profile";
+import validationReducer from "./validation";
+
+// Add the generated reducer as a specific top-level reducer
+const appReducer = combineReducers({
+  auth: authReducer,
+  profile: profileReducer,
+  validation: validationReducer,
+  [API.reducerPath]: API.reducer,
+});
+
+/*
+ adding condition to reset all store state
+ check store.js for action Creator definition
+ https://stackoverflow.com/questions/35622588/how-to-reset-the-state-of-a-redux-store?page=1&tab=votes#tab-top
+*/
+const rootReducer = (state, action) => {
+  if (action.type === "RESET_STORE") {
+    return appReducer(undefined, action);
+  }
+  return appReducer(state, action);
+};
+
+export default rootReducer;
--- src/features/store.js
+++ src/features/store.js
@@ -1,23 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { createAction, configureStore } from "@reduxjs/toolkit";
 // Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { API } from "../services/api";
-import authReducer from "./auth";
-import validationReducer from "./validation";
+import rootReducer from "./reducers";
 
+// action to clear the store
+export const resetStore = createAction("RESET_STORE");
+
+/*
+ Adding the api middleware enables caching, invalidation, polling,
+ and other useful features of `rtk-query`.
+ */
 export const store = configureStore({
-  reducer: {
-    // Add the generated reducer as a specific top-level slice
-    auth: authReducer,
-    validation: validationReducer,
-    [API.reducerPath]: API.reducer,
-  },
-  // Adding the api middleware enables caching, invalidation, polling,
-  // and other useful features of `rtk-query`.
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(API.middleware),
 });
 
-// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
-// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+/*
+ optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+ see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+*/
 setupListeners(store.dispatch);
