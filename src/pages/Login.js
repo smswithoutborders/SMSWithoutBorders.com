b176d5842f7c999c8cdb246e460fc6815068f48d
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-01-22 23:06:21 +0100

    refactor: login, remove commented code

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,7 +22,7 @@ import {
   PageAnimationWrapper,
 } from "components";
 
-// login schema used by react-hook-form an yup
+// login schema used by react-hook-form and yup
 const schema = yup.object().shape({
   phone_number: yup.string().required("Please Enter your Phone Number"),
   password: yup
@@ -71,98 +71,38 @@ const Login = () => {
     }
   }, [setValue, dispatch, navigate, auth.authKey]);
 
-  // const handleLogins = (data) => {
-  //   setLoading(true);
-  //   userLogin(data.phone_number, data.password)
-  //     .then((response) => {
-  //       if (response.status === 200) {
-  //         toast.success("Login successful \n You will be redirected shortly");
-
-  //         setTimeout(() => {
-  //           dispatch({
-  //             type: "LOGIN",
-  //             payload: {
-  //               id: response.data.id,
-  //               token: response.data.auth_key,
-  //             },
-  //           });
-  //         }, 1000);
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       if (error.response) {
-  //         /*
-  //          * The request was made and the server responded with a
-  //          * status code that falls out of the range of 2xx
-  //          */
-  //         switch (error.response.status) {
-  //           case 400:
-  //             toast.error(
-  //               "Something went wrong \n We are working to resolve this. Please try again"
-  //             );
-  //             break;
-  //           case 401:
-  //             toast.error(
-  //               "Forbidden, Account is unauthorized. \n Sign Up to create account"
-  //             );
-  //             break;
-  //           case 500:
-  //             toast.error(
-  //               "Something went wrong \n We are working to resolve this.  Please try again"
-  //             );
-  //             break;
-  //           default:
-  //             toast.error(
-  //               "Something went wrong \n We are working to resolve this.  Please try again"
-  //             );
-  //         }
-  //         setLoading(false);
-  //       } else if (error.request) {
-  //         /*
-  //          * The request was made but no response was received, `error.request`
-  //          * is an instance of XMLHttpRequest in the browser and an instance
-  //          * of http.ClientRequest in Node.js
-  //          */
-  //         setLoading(false);
-  //         toast.error(
-  //           "Network error \n Please check your network and try again"
-  //         );
-  //       } else {
-  //         // Something happened in setting up the request and triggered an Error
-  //         setLoading(false);
-  //         toast.error(
-  //           "Network error \n Please check your network and try again"
-  //         );
-  //       }
-  //     });
-  // };
-
   const handleLogin = async (data) => {
     // cache the data in case we need it later
     setCache(data);
     try {
       const user = await login(data).unwrap();
       // save user credentials to state
+      toast.success("Login successful");
       dispatch(saveAuth(user));
       // remove any cached data
       clearCache();
       /*
         redirect users if they initially tried to access a private route
         without permission
+        using setTimeOut to make sure state gets updated before redirects
       */
-      if (location.state && location.state.path) {
-        navigate(location.state.path);
-      } else {
-        navigate("/dashboard");
-      }
+      setTimeout(() => {
+        if (location.state && location.state.path) {
+          navigate(location.state.path);
+        } else {
+          navigate("/dashboard");
+        }
+      }, 1000);
     } catch (error) {
       switch (error.status) {
         case 400:
-          toast.error("An error occured. Please contact support");
+          toast.error(
+            "Something went wrong \n We are working to resolve this. Please try again"
+          );
           break;
         case 401:
           toast.error(
-            "Sorry you are not authorized to use this service. Please contact support"
+            "Forbidden, Account is unauthorized. \n check your phonenumber and password"
           );
           break;
         case 409:
@@ -170,7 +110,6 @@ const Login = () => {
             "There is a possible duplicate of this account please contact support"
           );
           break;
-
         case 429:
           toast.error(
             "Too many failed attempts please wait a while and try again"
@@ -179,6 +118,7 @@ const Login = () => {
         case 500:
           toast.error("A critical error occured. Please contact support");
           break;
+        // custom error thrown by RTK Query https://redux-toolkit.js.org/rtk-query/usage/error-handling
         case "FETCH_ERROR":
           toast.error("An error occured, please check your network try again");
           break;
@@ -192,14 +132,13 @@ const Login = () => {
     when making requests show loading indicator
     Also maintain after request is successfull to update background state
   */
-
   if (isLoading || isSuccess) {
     return <Loader />;
   }
 
   return (
     <PageAnimationWrapper>
-      <AuthContainer className="grid h-screen place-items-center">
+      <AuthContainer className="grid h-screen bg-gray-100 place-items-center">
         <div className="container flex flex-wrap items-center mx-auto">
           <div className="flex flex-col w-full p-8 m-4 mt-10 bg-white shadow-lg lg:w-2/6 md:w-1/2 rounded-xl md:mx-auto md:mt-0">
             <div className="mb-8">
@@ -249,7 +188,10 @@ const Login = () => {
               </Button>
             </form>
 
-            <Link to="/password-reset" className="mt-4 text-center cursor-pointer text-primary-800">
+            <Link
+              to="/password-reset"
+              className="mt-4 text-center cursor-pointer text-primary-800"
+            >
               Forgot Password
             </Link>
 
