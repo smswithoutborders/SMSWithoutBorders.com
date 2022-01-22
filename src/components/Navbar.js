50955f9b294ec2798ee1239454f61b07b1116497
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-01-22 16:25:23 +0100

    update: dashboard navbar component with new design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,130 +6,95 @@ import logo from "images/logo-icon-light.png";
 import { IoWalletOutline } from "react-icons/io5";
 import { IoMdSync } from "react-icons/io";
 import { Link } from "react-router-dom";
-import { useAppContext } from "App";
-import {
-  FiMenu,
-  FiInfo,
-  FiSettings,
-  FiUser,
-  FiLogOut,
-  FiExternalLink,
-} from "react-icons/fi";
+import { profileSelector, resetStore } from "features";
+import { useSelector, useDispatch } from "react-redux";
+import { FiMenu, FiX, FiLogOut, FiUser, FiSettings } from "react-icons/fi";
+import { Transition } from "@headlessui/react";
+import { clearCache } from "services";
+import toast from "react-hot-toast";
 
-const MainHeader = tw.header`flex justify-between items-center bg-white shadow-lg`;
-const NavContainer = tw.div`block lg:inline-flex`;
-const NavButton = tw.button`h-16 items-center transition duration-300 hocus:bg-primary-900 hocus:outline-none hocus:text-white text-gray-900 font-medium  px-6 py-3 no-underline appearance-none`;
 const NavLink = tw(
   Link
-)`w-full lg:w-max inline-flex h-16 transition duration-300 hocus:bg-primary-900 hocus:outline-none hocus:text-white text-gray-900 font-medium  px-6 py-3 no-underline items-center hocus:no-underline appearance-none`;
-const ExtLink = tw.a`w-full lg:w-max inline-flex h-16 transition duration-300 hocus:bg-primary-300 hocus:outline-none text-gray-900 font-medium hocus:font-bold px-6 py-3 no-underline items-center hocus:no-underline appearance-none`;
-const StartedExtLink = tw(ExtLink)`text-primary-900 bg-primary-300 font-bold`;
-const LogoLink = styled(NavLink)`
-  ${tw`inline-flex items-center ml-0! hocus:bg-white hocus:text-gray-900 font-bold text-xl`};
+)`flex hocus:outline-none text-gray-900 font-medium hocus:font-bold p-5 items-center appearance-none`;
+const ExtLink = tw.a`flex hocus:outline-none text-gray-900 font-medium hocus:font-bold p-5 items-center appearance-none`;
+const StartedExtLink = tw(ExtLink)`text-primary-800 font-medium`;
+const LogoLink = styled(Link)`
+  ${tw`flex items-center text-xl font-bold lg:ml-4`};
   img {
-    ${tw`w-8 h-8 mr-3`}
+    ${tw`w-10 h-10 mr-3`}
   }
 `;
-const UserActions = tw.div`flex flex-col lg:flex-row items-center`;
-const UserActionsButton = tw(
-  NavButton
-)`w-full lg:w-max flex flex-row items-center px-6 py-3 md:px-4 `;
-const MobileNav = tw.nav`lg:hidden flex flex-1 items-center justify-between`;
-const NavToggle = tw(
-  NavButton
-)`lg:hidden focus:outline-none transition duration-300 hocus:bg-white hocus:text-gray-900`;
-const DesktopNav = tw.nav`hidden lg:flex flex-1 justify-between items-center bg-white`;
+const UserActions = tw.div`flex items-center mt-20 justify-between bg-gray-100 lg:(bg-white mt-0)`;
+const NavContainer = tw.div`lg:flex`;
+const MobileNav = tw.nav`lg:hidden z-50 bg-white sticky top-0 shadow-lg`;
+const DesktopNav = tw.nav`hidden lg:flex  justify-between items-center bg-white h-16 shadow-lg`;
+const NavButton = tw.button`flex text-gray-900 font-medium hocus:(font-bold) p-5 items-center appearance-none`;
 
 export const Navbar = () => {
-  const [isAboutOpen, setIsAboutOpen] = useState(false);
   const [open, setOpen] = useState(false);
-  const { state, handleLogOut } = useAppContext();
-  const { userProfile } = state;
+  const dispatch = useDispatch();
+  const profile = useSelector(profileSelector);
 
+  // helper function for menu toggle and logout
   function toggleMenu() {
     setOpen(!open);
   }
 
-  const defaultLinks = [
-    <React.Fragment key="nav">
-      <NavContainer key={1}>
-        <StartedExtLink
-          onClick={() => toggleMenu()}
-          key="Get Started"
-          href="https://smswithoutborders.github.io/docs/intro"
-          target="_blank"
-        >
-          <FiExternalLink size={20} /> &nbsp; Get Started
-        </StartedExtLink>
-        <NavLink onClick={() => toggleMenu()} key="Profile" to="profile">
-          <FiUser size={20} /> &nbsp; Profile
-        </NavLink>
-        <NavLink onClick={() => toggleMenu()} key="Sync" to="profile">
-          <IoMdSync size={20} /> &nbsp; Sync
-        </NavLink>
-        <NavLink onClick={() => toggleMenu()} key="Wallet" to="wallet">
-          <IoWalletOutline size={20} /> &nbsp; Wallet(Store Access)
-        </NavLink>
-        <NavLink onClick={() => toggleMenu()} key="Settings" to="settings">
-          <FiSettings size={20} /> &nbsp; Settings
-        </NavLink>
-      </NavContainer>
-    </React.Fragment>,
-  ];
+  function handleLogOut() {
+    // clear store
+    dispatch(resetStore());
+    // clear local cache if any
+    clearCache();
+    toast.success("Logout successfull");
+  }
 
   const actionLinks = (
     <UserActions key={2}>
-      <UserActionsButton onClick={() => setIsAboutOpen(!isAboutOpen)}>
-        <FiInfo size={20} />
-      </UserActionsButton>
-      {userProfile && (
-        <UserActionsButton onClick={() => toggleMenu()}>
-          <div className="flex items-center">
-            <div className="flex items-center justify-center mr-2 bg-blue-100 rounded-full w-7 h-7">
-              <p className="font-bold text-center text-gray-800">
-                {userProfile?.name.charAt(0)}
+      {profile.name && (
+        <div onClick={() => toggleMenu()}>
+          <div className="flex items-center px-4 lg:px-0">
+            <div className="flex items-center justify-center mr-2 bg-blue-800 rounded-full w-9 h-9">
+              <p className="font-bold text-center text-white">
+                {profile?.name.charAt(0)}
               </p>
             </div>
+            <p className="mr-4 lg:hidden">{profile?.name}</p>
           </div>
-        </UserActionsButton>
+        </div>
       )}
-      <UserActionsButton onClick={() => handleLogOut()}>
+      <NavButton onClick={() => handleLogOut()}>
         <FiLogOut /> &nbsp; Logout
-      </UserActionsButton>
+      </NavButton>
     </UserActions>
   );
 
-  const mobileActionLinks = (
-    <UserActions key={2}>
-      <UserActionsButton
-        onClick={() => {
-          setOpen(false);
-          setIsAboutOpen(!isAboutOpen);
-        }}
+  const defaultLinks = (
+    <NavContainer>
+      <StartedExtLink
+        onClick={() => toggleMenu()}
+        key="Get Started"
+        href="https://smswithoutborders.github.io/docs/intro"
+        target="_blank"
       >
-        <FiInfo size={20} /> &nbsp; About
-      </UserActionsButton>
-      {userProfile && (
-        <UserActionsButton tw="px-5 md:px-3" onClick={() => toggleMenu()}>
-          {userProfile?.name}
-          <div className="flex items-center">
-            <div className="flex items-center justify-center mr-2 bg-blue-100 rounded-full w-7 h-7">
-              <p className="font-bold text-center text-gray-800">
-                {userProfile?.name.charAt(0)}
-              </p>
-            </div>
-            <p className="mr-4 text-sm">{userProfile?.name}</p>
-          </div>
-        </UserActionsButton>
-      )}
-      <UserActionsButton onClick={() => handleLogOut()}>
-        <FiLogOut /> &nbsp; Logout
-      </UserActionsButton>
-    </UserActions>
+        Get Started
+      </StartedExtLink>
+      <NavLink onClick={() => toggleMenu()} key="Profile" to="profile">
+        <FiUser size={20} /> &nbsp; Profile
+      </NavLink>
+      <NavLink onClick={() => toggleMenu()} key="Sync" to="profile">
+        <IoMdSync size={20} /> &nbsp; Sync
+      </NavLink>
+      <NavLink onClick={() => toggleMenu()} key="Wallet" to="wallet">
+        <IoWalletOutline size={20} /> &nbsp; Wallet(Store Access)
+      </NavLink>
+      <NavLink onClick={() => toggleMenu()} key="Settings" to="settings">
+        <FiSettings size={20} /> &nbsp; Settings
+      </NavLink>
+    </NavContainer>
   );
 
   const defaultLogoLink = (
-    <LogoLink to="/dashboard">
+    <LogoLink to="/">
       <img src={logo} alt="logo" />
       <span>SMSwithoutborders</span>
     </LogoLink>
@@ -137,23 +102,36 @@ export const Navbar = () => {
 
   return (
     <Fragment>
-      <MainHeader>
-        <DesktopNav>
+      <DesktopNav>
+        {defaultLogoLink}
+        {defaultLinks}
+        {actionLinks}
+      </DesktopNav>
+      <MobileNav>
+        <div className="flex items-center justify-between p-4">
           {defaultLogoLink}
-          {defaultLinks}
-          {actionLinks}
-        </DesktopNav>
-        <MobileNav>
-          {defaultLogoLink}
-          <NavToggle onClick={() => toggleMenu()}>
-            <FiMenu size={24} />
-          </NavToggle>
-          <Fragment>
+          <button className="appearance-none" onClick={() => toggleMenu()}>
+            {open ? <FiX size={24} /> : <FiMenu size={24} />}
+          </button>
+        </div>
+
+        {open && (
+          <Transition
+            show={open}
+            appear={true}
+            enter="transition-opacity duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            className="flex flex-col w-full h-screen bg-white"
+          >
             {defaultLinks}
-            {mobileActionLinks}
-          </Fragment>
-        </MobileNav>
-      </MainHeader>
+            {actionLinks}
+          </Transition>
+        )}
+      </MobileNav>
     </Fragment>
   );
 };
