6375e6e6d0b152397d6b43090330943b2e185cb3
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-02-09 14:58:55 +0100

    refactor: update account deletion page layout

--- src/pages/settings/AccountDeletion.js
+++ src/pages/settings/AccountDeletion.js
@@ -5,17 +5,10 @@ import { authSelector, resetStore } from "features";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { FiTrash2 } from "react-icons/fi";
-import {
-  PageAnimationWrapper,
-  PasswordInput,
-  FormGroup,
-  Button,
-  Loader,
-  useTitle,
-} from "components";
+import { PasswordInput, FormGroup, Button, Loader, useTitle } from "components";
 
 const AccountDeletion = () => {
-  useTitle("password reset");
+  useTitle("Account Deletion");
   const auth = useSelector(authSelector);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -40,31 +33,41 @@ const AccountDeletion = () => {
       // redirect to login
       navigate("/login");
     } catch (error) {
-      switch (error.status) {
-        case 400:
-          toast.error(
-            "Something went wrong \n We are working to resolve this. Please try again"
-          );
-          break;
-        case 401:
-          toast.error(
-            "Sorry We did not find your account \n please sign up to create one"
-          );
-          break;
-        case 409:
-          toast.error(
-            "An account with this number already exists.Please Log In instead"
-          );
-          break;
-        case 500:
-          toast.error("A critical error occured. Please contact support");
-          break;
-        // custom error thrown by RTK Query https://redux-toolkit.js.org/rtk-query/usage/error-handling
-        case "FETCH_ERROR":
-          toast.error("An error occured, please check your network try again");
-          break;
-        default:
-          toast.error("An error occured, please try again");
+      // https://redux-toolkit.js.org/rtk-query/usage/error-handling
+      const { status, originalStatus } = error;
+      if (originalStatus) {
+        switch (originalStatus) {
+          case 400:
+            toast.error(
+              "Something went wrong \n We are working to resolve this. Please try again"
+            );
+            break;
+          case 401:
+            toast.error("Sorry you are unauthorized. Please login");
+            break;
+          case 403:
+            toast.error("Forbidden, password is invalid");
+            break;
+          case 409:
+            toast.error(
+              "There is a possible duplicate of this account please contact support"
+            );
+            break;
+          case 429:
+            toast.error(
+              "Too many failed attempts please wait a while and try again"
+            );
+            break;
+          case 500:
+            toast.error("A critical error occured. Please contact support");
+            break;
+          default:
+            toast.error(
+              "An error occured, please check your network try again"
+            );
+        }
+      } else if (status === "FETCH_ERROR") {
+        toast.error("An error occured, please check your network try again");
       }
     }
   }
@@ -78,39 +81,37 @@ const AccountDeletion = () => {
   }
 
   return (
-    <PageAnimationWrapper>
-      <div className="max-w-lg p-4 mx-auto text-center">
-        <h1 className="mb-4 text-3xl font-bold text-red-600">Delete Account</h1>
-        <p>
-          This action cannot be reversed. Please enter your password to confirm
-        </p>
+    <div className="max-w-screen-sm py-20 mx-auto text-center md:px-8">
+      <h1 className="mb-4 text-3xl font-bold text-red-600">Delete Account</h1>
+      <p>
+        This action cannot be reversed. Please enter your password to confirm
+      </p>
 
-        <div className="flex-1 w-full mt-8">
-          <form
-            className="px-4 mx-auto sm:px-3"
-            onSubmit={(evt) => handleDeletion(evt)}
+      <div className="max-w-md mx-auto mt-12">
+        <form
+          className="px-4 mx-auto sm:px-3"
+          onSubmit={(evt) => handleDeletion(evt)}
+        >
+          <FormGroup>
+            <PasswordInput
+              name="password"
+              placeholder="password"
+              min={8}
+              required
+              onChange={(evt) => setPassword(evt.target.value)}
+            />
+          </FormGroup>
+
+          <Button
+            className="bg-red-600 focus:bg-red-700 disabled:bg-gray-300"
+            type="submit"
+            disabled={password?.length >= 8 ? false : true}
           >
-            <FormGroup>
-              <PasswordInput
-                name="password"
-                placeholder="password"
-                min={8}
-                required
-                onChange={(evt) => setPassword(evt.target.value)}
-              />
-            </FormGroup>
-
-            <Button
-              className="bg-red-600 focus:bg-red-700 disabled:bg-gray-300"
-              type="submit"
-              disabled={password?.length >= 8 ? false : true}
-            >
-              <FiTrash2 size={20} /> &nbsp; delete
-            </Button>
-          </form>
-        </div>
+            <FiTrash2 size={20} /> &nbsp; delete
+          </Button>
+        </form>
       </div>
-    </PageAnimationWrapper>
+    </div>
   );
 };
 
