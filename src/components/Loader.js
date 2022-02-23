0353b5a48e5cef5cb3db19c562c2e82395ae6bdd
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-02-23 16:13:46 +0100

    update: add splashscreen

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,15 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import clsx from "clsx";
+import logo from "images/logo-icon-light.png";
 
-const LoadingContainer = styled.div.attrs({
-  className: "grid place-items-center text-gray-600 mx-auto w-full",
-})``;
+const LoadingContainer = styled.div.attrs(({ className }) => ({
+  className: clsx(
+    "grid place-items-center text-gray-600 mx-auto w-full",
+    className
+  ),
+}))``;
 
-const Spinner = styled.div.attrs({
-  className:
+const Spinner = styled.div.attrs(({ className }) => ({
+  className: clsx(
     "animate-spin rounded-full h-16 w-16 border-b-2 border-gray-600 mb-4",
-})``;
+    className
+  ),
+}))``;
 
 export const Loader = ({ message }) => {
   return (
@@ -28,6 +35,17 @@ export const InlineLoader = ({ message }) => {
       <div>
         <Spinner className="mx-auto" />
         <p className="mt-2">{message || "processing please wait"}</p>
+      </div>
+    </LoadingContainer>
+  );
+};
+
+export const SplashScreen = () => {
+  return (
+    <LoadingContainer className="h-screen">
+      <div className="flex items-center text-xl animate-pulse">
+        <img className="w-12 h-12 mr-3 md:w-20 md:h-20" src={logo} alt="logo" />
+        <h1 className="text-2xl font-black md:text-4xl">SMSWithoutBorders</h1>
       </div>
     </LoadingContainer>
   );
