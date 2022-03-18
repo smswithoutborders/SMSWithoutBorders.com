1ed688478afdf333a794fec36564af3cec6efe49
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-03-18 14:44:34 +0100

    refactor: Add translation tokens for sync page

--- src/pages/Sync.js
+++ src/pages/Sync.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { IoMdSync } from "react-icons/io";
 import { authSelector } from "features";
 import { useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
 import { useSyncQuery, useGetPlatformsQuery } from "services";
 import {
   Alert,
@@ -15,7 +16,8 @@ import {
 } from "components";
 
 const Sync = () => {
-  useTitle("Synchronize Accounts");
+  const { t } = useTranslation();
+  useTitle(t("sync.page-title"));
   const auth = useSelector(authSelector);
   const { data: platforms = {} } = useGetPlatformsQuery(auth);
   const { savedPlatforms = [] } = platforms;
@@ -28,9 +30,9 @@ const Sync = () => {
 
   useEffect(() => {
     if (isError) {
-      toast.error("Sorry an error occured please try again");
+      toast.error(t("error-messages.general-error-message"));
     }
-  }, [isError]);
+  }, [isError, t]);
 
   // Only allow sync if at least 1 platform is saved
   if (!savedPlatforms.length) {
@@ -39,23 +41,24 @@ const Sync = () => {
         <div className="grid max-w-screen-md min-h-screen grid-cols-2 px-6 py-20 mx-auto prose md:px-8">
           <div className="text-center col-span-full">
             <h1 className="inline-flex items-center mb-0 text-4xl font-bold">
-              <IoMdSync size={48} className="mr-2" /> Synchronize Accounts
+              <IoMdSync size={48} className="mr-2" />
+              <span>{t("sync.heading")}</span>
             </h1>
 
             <div className="max-w-screen-sm mx-auto my-4">
               <Alert
                 kind="primary"
-                message="Sorry it seems you havent stored any tokens"
+                message={t("sync.alerts.no-platforms")}
                 hideCloseButton
               />
             </div>
             <div className="my-8">
-              <span>Please click on the link below to store tokens &nbsp; </span>
+              <span>{t("sync.section-1.details")} &nbsp;</span>
               <details>
-                <summary className="text-blue-800">learn more</summary>
-                Synchronization becomes available only after you perform an
-                action that updates your security configurations. An example is
-                storing access in the wallet.
+                <summary className="text-blue-800">
+                  {t("sync.section-1.summary.caption")}
+                </summary>
+                {t("sync.section-1.summary.details")}
               </details>
             </div>
 
@@ -63,7 +66,7 @@ const Sync = () => {
               to="/dashboard/wallet"
               className="inline-flex items-center justify-center px-6 py-2 text-white no-underline bg-blue-800 rounded-lg outline-none focus:outline-none hover:bg-blue-900"
             >
-              Store Access Now
+              {t("sync.section-1.cta-button-text")}
             </Link>
           </div>
         </div>
@@ -76,23 +79,17 @@ const Sync = () => {
       <div className="grid max-w-screen-xl min-h-screen grid-cols-2 px-6 py-20 mx-auto prose md:px-8">
         <div className="col-span-full lg:col-span-1">
           <h1 className="inline-flex items-center mb-0 text-4xl font-bold">
-            <IoMdSync size={48} className="mr-2" /> Synchronize Accounts
+            <IoMdSync size={48} className="mr-2" />
+            <span>{t("sync.heading")}</span>
           </h1>
 
-          <p>This lets you use the SW/OB secure gateway for all messages</p>
-          <p>
-            Synchronization becomes available only after you perform an action
-            that updates your security configurations. An example is storing
-            access in the wallet.
-          </p>
-          <p>
-            Please follow these steps to sychronize your saved access with
-            SMSwithoutborders mobile app.
-          </p>
+          <p>{t("sync.section-2.paragraph-1")}</p>
+          <p>{t("sync.section-2.paragraph-2")}</p>
+          <p>{t("sync.section-2.paragraph-3")}</p>
           <ol>
-            <li>Open SMSWithoutBorders app on your phone</li>
-            <li>Tap the Synchronize button</li>
-            <li>Point your phone to this screen to capture the code</li>
+            <li>{t("sync.section-2.sync-steps.1")}</li>
+            <li>{t("sync.section-2.sync-steps.2")}</li>
+            <li>{t("sync.section-2.sync-steps.3")}</li>
           </ol>
         </div>
 
@@ -107,7 +104,7 @@ const Sync = () => {
             <div className="mx-auto border w-[300px] h-[300px] rounded-lg shadow p-2 grid place-items-center">
               <Button className="mt-4" onClick={() => window.location.reload()}>
                 <IoMdSync size={22} />
-                <span className="ml-1">sync</span>
+                <span className="ml-1">{t("labels.sync")}</span>
               </Button>
             </div>
           ) : (
@@ -116,8 +113,10 @@ const Sync = () => {
 
           <div className="mx-auto text-center">
             <p className="font-bold text-md">
-              Status : &nbsp;
-              <span className="font-normal">{status}</span>
+              <span>{t("sync.section-2.status.heading")} : </span> &nbsp;
+              <span className="font-normal">
+                {t(`sync.section-2.status.${status}`)}
+              </span>
             </p>
           </div>
         </div>
