fcac826e252009024fb8cfab275ca1313006fbb5
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-03-18 10:55:05 +0100

    refactor: add translation tokens to contact and dasbhoard pages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { FiMail, FiGlobe, FiMessageSquare } from "react-icons/fi";
 import { PageAnimationWrapper } from "components";
+import { useTranslation } from "react-i18next";
 
 const Contact = () => {
+  const { t } = useTranslation();
   return (
     <PageAnimationWrapper>
       <div className="max-w-screen-xl p-6 mx-auto my-10 prose text-gray-900 lg:mt-20 md:p-8">
         <div className="lg:text-center">
-          <h1 className="">Contact Us</h1>
-          <p className="">
-            Using SWOB is as easy as authenticating your credentials
-          </p>
+          <h1 className="">{t("contact.heading")}</h1>
+          <p className="">{t("contact.details")}</p>
         </div>
 
         <div className="w-full my-10 lg:flex lg:justify-evenly lg:items-center">
@@ -19,7 +19,7 @@ const Contact = () => {
               <FiMail className="w-7 h-7 lg:w-10 lg:h-10" />
             </div>
             <div className="">
-              <p className="my-0">Email</p>
+              <p className="my-0">{t("contact.email")}</p>
               <h4 className="my-0">
                 <a href="mailto:developers@smswithoutborders.com">
                   developers@smswithoutborders.com
@@ -41,7 +41,7 @@ const Contact = () => {
               <FiGlobe className="w-7 h-7 lg:w-10 lg:h-10" />
             </div>
             <div className="">
-              <p className="my-0">website</p>
+              <p className="my-0">{t("contact.website")}</p>
               <h4 className="my-0">
                 <a
                   href="https://smswithoutborders.com"
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,11 +5,13 @@ import { BsPersonCircle } from "react-icons/bs";
 import { useSelector } from "react-redux";
 import { useGetMetricsQuery } from "services";
 import { authSelector } from "features";
+import { useTranslation } from "react-i18next";
 import { formatUTCDate } from "utils";
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
-  useTitle("Dashboard");
+  const { t } = useTranslation();
+  useTitle(t("dashboard.page-title"));
   const auth = useSelector(authSelector);
   /*
    Once this query runs metrics is also updated in state
@@ -30,12 +32,9 @@ const Dashboard = () => {
   if (isError) {
     return (
       <div className="p-8 my-24 prose">
-        <h2>An error occured</h2>
-        <p className="">
-          Sorry we could not locate your metrics. If error persists, please
-          contact support
-        </p>
-        <Button onClick={() => refetch()}>try again</Button>
+        <h2>{t("error-messages.general-error-title")}</h2>
+        <p className="">{t("dashboard.alerts.load-error")}</p>
+        <Button onClick={() => refetch()}>{t("labels.try-again")}</Button>
       </div>
     );
   }
@@ -45,18 +44,20 @@ const Dashboard = () => {
       <div className="p-8 mx-auto my-24 prose text-center">
         <BsPersonCircle size={96} className="inline-block mb-8" />
         <h1 className="text-3xl">
-          Hello!, <span className="font-medium">Welcome</span>
+          <span>{t("dashboard.greeting-1")}, </span>
+          <span className="font-medium">{t("dashboard.greeting-2")}</span>
         </h1>
         <div className="my-8">
-          <h3>Join Date</h3>
+          <h3>{t("dashboard.join-date")}</h3>
           <p>{formatUTCDate(metrics?.createdAt)}</p>
-          <h3>Last Login</h3>
+          <h3>{t("dashboard.last-login")}</h3>
           <p>{formatUTCDate(metrics?.updatedAt)}</p>
           <Link
             to="/dashboard/sync"
             className="inline-flex items-center px-8 py-2 my-4 text-white no-underline bg-blue-800 rounded-md appearance-none hover:bg-blue-900"
           >
-            <IoMdSync size={20} /> &nbsp; sync
+            <IoMdSync size={20} />
+            <span className="ml-1">{t("labels.sync")}</span>
           </Link>
         </div>
       </div>
