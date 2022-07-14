3304f05a4b8ada51b55a92688ed386491ac508f4
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-07-14 16:09:21 +0100

    update: change locale to french when fr param is used for login and signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,7 +5,7 @@ import { FiLogIn } from "react-icons/fi";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { useNavigate, useLocation, Link } from "react-router-dom";
+import { useNavigate, useLocation, Link, useParams } from "react-router-dom";
 import { useLoginMutation } from "services";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
@@ -25,8 +25,9 @@ import {
 } from "components";
 
 const Login = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   useTitle(t("login.page-title"));
+  const { lang } = useParams();
 
   // check if recaptcha is enabled and conditionally add validation
   const ENABLE_RECAPTCHA =
@@ -67,8 +68,12 @@ const Login = () => {
   const auth = useSelector(authSelector);
 
   useEffect(() => {
+    // check locale
+    if (lang === "fr") {
+      i18n.changeLanguage("fr");
+    }
     // if logged in then redirect to dashboard
-    if (auth.uid && location.state && location.state.path) {
+    else if (auth.uid && location.state && location.state.path) {
       /*
         redirect users if they initially tried to access a private route
         without permission
@@ -77,7 +82,7 @@ const Login = () => {
     } else if (auth.uid) {
       navigate("/dashboard");
     }
-  }, [setValue, dispatch, navigate, auth.uid, location.state]);
+  }, [setValue, dispatch, navigate, auth.uid, location.state, lang, i18n]);
 
   const handleLogin = async (data) => {
     try {
