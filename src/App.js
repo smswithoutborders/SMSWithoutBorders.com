8a8d0d1269d2aee312720372a5da8f639f43f96a
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-08-01 12:28:38 +0100

    update: set privacy policy locale from url

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { Toaster } from "react-hot-toast";
 import { FiSettings } from "react-icons/fi";
 import { useTranslation } from "react-i18next";
 
-
-
 const Sync = lazy(() => import("pages/Sync"));
 const Contact = lazy(() => import("pages/Contact"));
 const Privacy = lazy(() => import("pages/Privacy"));
@@ -23,12 +21,24 @@ const Downloads = lazy(() => import("pages/Downloads"));
 const AccountDeletion = lazy(() => import("pages/settings/AccountDeletion"));
 const PasswordChange = lazy(() => import("pages/settings/PasswordChange"));
 const DashboardLayout = lazy(() => import("pages/DashboardLayout"));
-const TelegramRegistration = lazy(() => import("pages/Wallet/telegram/TelegramRegistration"));
-const SignupCodeVerification = lazy(() => import("pages/signup/CodeVerification"));
-const PhoneNumberVerification = lazy(() => import("pages/password-reset/PhoneNumberVerification"));
-const TelegramCodeVerification = lazy(() => import("pages/Wallet/telegram/CodeVerification"));
-const TelegramNumberVerification = lazy(() => import("pages/Wallet/telegram/PhoneNumberVerification"));
-const PasswordChangeVerification = lazy(() => import("pages/password-reset/CodeVerification"));
+const TelegramRegistration = lazy(() =>
+  import("pages/Wallet/telegram/TelegramRegistration")
+);
+const SignupCodeVerification = lazy(() =>
+  import("pages/signup/CodeVerification")
+);
+const PhoneNumberVerification = lazy(() =>
+  import("pages/password-reset/PhoneNumberVerification")
+);
+const TelegramCodeVerification = lazy(() =>
+  import("pages/Wallet/telegram/CodeVerification")
+);
+const TelegramNumberVerification = lazy(() =>
+  import("pages/Wallet/telegram/PhoneNumberVerification")
+);
+const PasswordChangeVerification = lazy(() =>
+  import("pages/password-reset/CodeVerification")
+);
 const PasswordReset = lazy(() => import("pages/password-reset/PasswordReset"));
 
 // toast notifications: https://uxdesign.cc/toasts-or-snack-bars-design-organic-system-notifications-1236f2883023
@@ -46,75 +56,84 @@ const App = () => {
         }}
       />
       <Suspense fallback={<SplashScreen />}>
-      <BrowserRouter>
-        <ScrollToTop>
-          <Routes>
-            <Route path="/" element={<Website />}>
-              <Route index element={<Landing />} />
-              <Route path="login">
-                <Route index element={<Login />} />
-                <Route path=":lang" element={<Login />} />
-              </Route>
-              <Route path="downloads" element={<Downloads />} />
-              <Route path="privacy-policy" element={<Privacy />} />
-              <Route path="contact-us" element={<Contact />} />
-              <Route path="sign-up">
-                <Route index element={<Signup />} />
-                <Route path=":lang" element={<Signup />} />
-                <Route path="verify" element={<SignupCodeVerification />} />
-              </Route>
-              <Route path="password-reset">
-                <Route index element={<PhoneNumberVerification />} />
-                <Route path="verify" element={<PasswordChangeVerification />} />
-                <Route path="reset" element={<PasswordReset />} />
-              </Route>
-            </Route>
-
-            <Route
-              path="dashboard"
-              element={
-                <RequireAuth>
-                  <DashboardLayout />
-                </RequireAuth>
-              }
-            >
-              <Route index element={<Wallet />} />
-              <Route path="metrics" element={<Dashboard />} />
-              <Route path="sync" element={<Sync />} />
-              <Route path="wallet">
-                <Route index element={<Wallet />} />
-                <Route path="telegram">
-                  <Route index element={<TelegramNumberVerification />} />
-                  <Route path="verify" element={<TelegramCodeVerification />} />
-                  <Route path="register" element={<TelegramRegistration />} />
+        <BrowserRouter>
+          <ScrollToTop>
+            <Routes>
+              <Route path="/" element={<Website />}>
+                <Route index element={<Landing />} />
+                <Route path="login">
+                  <Route index element={<Login />} />
+                  <Route path=":lang" element={<Login />} />
+                </Route>
+                <Route path="downloads" element={<Downloads />} />
+                <Route path="privacy-policy">
+                  <Route index element={<Privacy />} />
+                  <Route path=":lang" element={<Privacy />} />
+                </Route>
+                <Route path="contact-us" element={<Contact />} />
+                <Route path="sign-up">
+                  <Route index element={<Signup />} />
+                  <Route path=":lang" element={<Signup />} />
+                  <Route path="verify" element={<SignupCodeVerification />} />
+                </Route>
+                <Route path="password-reset">
+                  <Route index element={<PhoneNumberVerification />} />
+                  <Route
+                    path="verify"
+                    element={<PasswordChangeVerification />}
+                  />
+                  <Route path="reset" element={<PasswordReset />} />
                 </Route>
               </Route>
-              <Route path="settings" element={<Settings />}>
-                <Route
-                  index
-                  element={
-                    <div className="grid place-items-center">
-                      <div className="p-8 text-center h-80">
-                        <FiSettings size={100} className="mx-auto mb-4" />
-                        <p className="mb-8 text-base leading-relaxed">
-                          {t("settings.paragraph")}
-                        </p>
+
+              <Route
+                path="dashboard"
+                element={
+                  <RequireAuth>
+                    <DashboardLayout />
+                  </RequireAuth>
+                }
+              >
+                <Route index element={<Wallet />} />
+                <Route path="metrics" element={<Dashboard />} />
+                <Route path="sync" element={<Sync />} />
+                <Route path="wallet">
+                  <Route index element={<Wallet />} />
+                  <Route path="telegram">
+                    <Route index element={<TelegramNumberVerification />} />
+                    <Route
+                      path="verify"
+                      element={<TelegramCodeVerification />}
+                    />
+                    <Route path="register" element={<TelegramRegistration />} />
+                  </Route>
+                </Route>
+                <Route path="settings" element={<Settings />}>
+                  <Route
+                    index
+                    element={
+                      <div className="grid place-items-center">
+                        <div className="p-8 text-center h-80">
+                          <FiSettings size={100} className="mx-auto mb-4" />
+                          <p className="mb-8 text-base leading-relaxed">
+                            {t("settings.paragraph")}
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                  }
-                />
-                <Route path="change-password" element={<PasswordChange />} />
-                <Route path="delete-account" element={<AccountDeletion />} />
+                    }
+                  />
+                  <Route path="change-password" element={<PasswordChange />} />
+                  <Route path="delete-account" element={<AccountDeletion />} />
+                </Route>
               </Route>
-            </Route>
-            <Route
-              path="platforms/:platform/protocols/:protocol/redirect_codes"
-              element={<WalletRedirect />}
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </ScrollToTop>
-      </BrowserRouter>
+              <Route
+                path="platforms/:platform/protocols/:protocol/redirect_codes"
+                element={<WalletRedirect />}
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </ScrollToTop>
+        </BrowserRouter>
       </Suspense>
     </Fragment>
   );
