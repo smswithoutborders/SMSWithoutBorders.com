1487e87b62c1a9e9da9eba36fdc52adde7eab027
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-10-04 16:29:39 +0100

    update(wallet): inform users to open twitter app when storing on mobile

--- src/pages/Wallet/components/Info.js
+++ src/pages/Wallet/components/Info.js
@@ -2,11 +2,17 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import GmailAuthScreen from "images/gmail-auth-screen.png";
 import { useTranslation } from "react-i18next";
+import { useDeviceDetection } from "hooks";
 
 // component for displaying additional platform information
 const Info = ({ platform }) => {
+
+  // we only need to show twitter info on mobile devices
+  const isMobile = useDeviceDetection();
+
   const views = {
     gmail: Gmail,
+    twitter: isMobile ? Twitter : null,
   };
 
   const InfoView = views[platform] ?? null;
@@ -26,7 +32,18 @@ const Gmail = () => {
   );
 };
 
+const Twitter = () => {
+  const { t } = useTranslation();
+  return (
+    <Fragment>
+      <h3>{t("wallet.labels.how-to-save")}</h3>
+      <p>{t("wallet.info.twitter")}</p>
+    </Fragment>
+  );
+};
+
 Info.propTypes = {
   platform: PropTypes.string,
 };
+
 export default Info;
