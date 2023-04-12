813a09d85a588c02ba9bcd7fbe8fd59709d7da70
Minion <nessakristo@gmail.com>
2023-04-12 02:39:50 +0100

    feat: added steps to dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -10,6 +10,7 @@ import { useTranslation } from "react-i18next";
 import { formatUTCDate } from "utils";
 import { Link } from "react-router-dom";
 import Error from "./Error";
+import VerticalLinearStepper from "./Stepper";
 
 const Dashboard = () => {
   const { t } = useTranslation();
@@ -39,12 +40,14 @@ const Dashboard = () => {
 
   return (
     <PageAnimationWrapper>
-      <div className="p-8 mx-auto my-24 prose text-center">
+      <div className="p-8 mx-auto my-7 prose text-center">
         <BsPersonCircle size={96} className="inline-block mb-8" />
         <h1 className="text-3xl">
           <span>{t("dashboard.greeting-1")}, </span>
           <span className="font-medium">{t("dashboard.greeting-2")}</span>
         </h1>
+        <h2>{t("dashboard.start")}</h2>
+        <VerticalLinearStepper />
         <div className="my-8">
           <h3>{t("dashboard.join-date")}</h3>
           <p>{formatUTCDate(metrics?.createdAt)}</p>
---
+++ src/pages/Stepper.js
@@ -0,0 +1,115 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import StepContent from "@mui/material/StepContent";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import { useTranslation } from "react-i18next";
+
+const steps = [
+  {
+    labelKey: "dashboard.label1",
+    descriptionKey: "dashboard.description1",
+    link: "https://example.com/step1",
+  },
+  {
+    labelKey: "dashboard.label2",
+    descriptionKey: "dashboard.description2",
+    link: "https://example.com/step2",
+  },
+  {
+    labelKey: "dashboard.label3",
+    descriptionKey: "dashboard.description3",
+    link: "https://example.com/step3",
+  },
+  {
+    labelKey: "dashboard.label4",
+    descriptionKey: "dashboard.description4",
+    link: "https://example.com/step4",
+  },
+];
+
+const ACTIVE_STEP_KEY = "activeStep";
+
+export default function VerticalLinearStepper() {
+  const [activeStep, setActiveStep] = React.useState(() => {
+    const storedStep = localStorage.getItem(ACTIVE_STEP_KEY);
+    return storedStep !== null ? parseInt(storedStep, 10) : 0;
+  });
+  const { t } = useTranslation();
+
+  React.useEffect(() => {
+    localStorage.setItem(ACTIVE_STEP_KEY, activeStep);
+  }, [activeStep]);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const handleLinkClick = (link) => {
+    window.location.href = link;
+    handleNext();
+  };
+
+  return (
+    <Box sx={{ maxWidth: 400 }}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((step, index) => (
+          <Step key={step.labelKey}>
+            <StepLabel
+              optional={
+                index === 3 ? (
+                  <Typography variant="caption">Last step</Typography>
+                ) : null
+              }
+            >
+              {t(step.labelKey)}
+            </StepLabel>
+            <StepContent>
+              <Typography>{t(step.descriptionKey)}</Typography>
+              <Box sx={{ mb: 2 }}>
+                <div>
+                  <Button
+                    variant="contained"
+                    onClick={() => handleLinkClick(step.link)}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    {index === steps.length - 1
+                      ? t("dashboard.finish")
+                      : t("dashboard.continue")}
+                  </Button>
+                  <Button
+                    disabled={index === 0}
+                    onClick={handleBack}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    {t("dashboard.back")}
+                  </Button>
+                </div>
+              </Box>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>{t("dashboard.completed")}</Typography>
+          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+            {t("dashboard.reset")}
+          </Button>
+        </Paper>
+      )}
+    </Box>
+  );
+}
